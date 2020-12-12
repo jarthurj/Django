89fb00003a58e32a59549c0cc6e9692ff983b3d3
Johnny <jaoldaker1@gmail.com>
2020-12-12 12:20:11 -0500

    a folder for writing out and testing functions by themselces

---
+++ random_test_files/age.py
@@ -0,0 +1,28 @@
+from datetime import date
+
+def age_verification(birthday):
+	today = date.today()
+	year_diff = today.year - birthday.year
+
+	if year_diff < 13:
+		print("less than 13 years")
+		return False
+
+	else:
+		month_diff = today.month - birthday.month
+		if month_diff < 0:
+			print("turning 13 this year")
+			return False
+		else:
+			day_diff = today.day - birthday.day 
+			if day_diff > 0:
+				print("turning 13 this month")
+				return False
+			else:
+				return True
+
+
+birthday = date.fromisoformat('2007-12-12')
+
+age_verification(birthday)
+
